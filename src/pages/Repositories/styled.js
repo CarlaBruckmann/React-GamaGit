c27b01a8cd67c250021e37b9c641dd0141820998
Carla Bruckmann <carlaabruckmann@gmail.com>
2022-01-25 12:17:24 -0300

    feat(pages) refactor component styles

--- src/pages/Repositories/styled.js
+++ src/pages/Repositories/styled.js
@@ -6,31 +6,57 @@ export const Container = styled.div`
   max-width: 991px;
   margin: 0 auto;
 `;
+export const Card = styled.div`
+  width: auto;
+  height: auto;
+  padding: 40px;
+  margin: 5% 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 2px solid var(--purple);
+  border-radius: 15px;
+`;
 export const Title = styled.h1`
+  font-weight: var(--font-medium);
   text-align: center;
-  font-size: 2rem;
-  font-family: sans-serif;
-  color: #333;
+  margin: 10px 0 20px 0;
 `;
 export const List = styled.ul`
   list-style: none;
   padding: 0;
-  font-family: sans-serif;
+  font-weight: var(--font-regular);
+  width: 90%;
 `;
 export const ListItem = styled.li`
   margin: 0.5rem 0;
-  background: #000;
-  color: #fff;
-  padding: 0.5rem;
+  background: var(--white-light);
+  border-radius: 10px;
+  padding: 0.7rem;
 `;
 export const LinkHome = styled(Link)`
   display: block;
-  width: 4rem;
+  width: auto;
   text-align: center;
   margin: 2rem auto;
-  background-color: #000;
-  padding: 10px 0;
-  color: #fff;
+  background-color: var(--purple);
+  padding: 10px 15px;
+  border-radius: 10px;
+  color: var(--white-bold);
   text-decoration: none;
-  font-family: sans-serif;
+  font-weight: var(--font-medium);
+  font-size: 15px;
+  &:focus,
+  &:active {
+    outline: none;
+    box-shadow: none;
+  }
+  &:hover {
+    transform: scale(1.05);
+    transition: 0.1s ease all;
+  }
 `;
